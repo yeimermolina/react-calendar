db25f26e3cbcfb82cc1f04ee0645ba0e195ccdf5
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2018-09-09 17:21:26 -0500

    Add Menu and Calendar containers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import Layout from './hoc/layout';
+import Menu from './containers/menu'
+import Calendar from './containers/calendar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Layout>
+          <Switch>
+              <Route path="/calendar" component= {Calendar}/>
+              <Route path="/" component= {Menu}/>
+              <Redirect to="/"/>
+          </Switch>
+        </Layout>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
